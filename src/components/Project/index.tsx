2972401e56a0317dd3290fccb94659073ec6809a
bushra-rauf <bush_rauf@yahoo.com>
2025-09-16 09:56:31 +0200

    added project subtitle

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -18,9 +18,9 @@ const Projects = () => {
             className="flex flex-col justify-between p-6 bg-white rounded-2xl shadow-md border border-indigo-100 hover:shadow-lg hover:-translate-y-1 transition group"
           >
             <h3 className="text-xl font-semibold text-gray-800 group-hover:text-indigo-600 transition">
-              Zoo App
+              Zoo Project
             </h3>
-            <p className="text-sm text-gray-500 mt-2">Personal Project</p>
+            <p className="text-sm text-gray-500 mt-2">Developed a Zoo project using React Vite with custom CSS for styling</p>
           </Link>
 
           {/* Project 2 */}
@@ -33,7 +33,7 @@ const Projects = () => {
             <h3 className="text-xl font-semibold text-gray-800 group-hover:text-indigo-600 transition">
               Restaurant App
             </h3>
-            <p className="text-sm text-gray-500 mt-2">Personal Project</p>
+            <p className="text-sm text-gray-500 mt-2">Built a responsive layout with Material UI</p>
           </Link>
 
           {/* Project 3 */}
@@ -46,7 +46,7 @@ const Projects = () => {
             <h3 className="text-xl font-semibold text-gray-800 group-hover:text-indigo-600 transition">
               Learn State App
             </h3>
-            <p className="text-sm text-gray-500 mt-2">Personal Project</p>
+            <p className="text-sm text-gray-500 mt-2">Implemented a responsive layout with Styled Component</p>
           </Link>
 
           {/* Group Project 1 */}
@@ -59,7 +59,7 @@ const Projects = () => {
             <h3 className="text-xl font-semibold text-gray-800 group-hover:text-indigo-600 transition">
               Rosvik Website
             </h3>
-            <p className="text-sm text-gray-500 mt-2">Group Project</p>
+            <p className="text-sm text-gray-500 mt-2">Group Project Responsive layout and clean components architecture with Tillwind</p>
           </Link>
 
           {/* Group Project 2 */}
@@ -72,7 +72,7 @@ const Projects = () => {
             <h3 className="text-xl font-semibold text-gray-800 group-hover:text-indigo-600 transition">
               GP-3 Project 1
             </h3>
-            <p className="text-sm text-gray-500 mt-2">Group Project</p>
+            <p className="text-sm text-gray-500 mt-2">Collaborative UI built with React and Tailwind CSS</p>
           </Link>
 
           {/* Group Project 3 */}
@@ -85,7 +85,7 @@ const Projects = () => {
             <h3 className="text-xl font-semibold text-gray-800 group-hover:text-indigo-600 transition">
               Australia Zoo Team Project
             </h3>
-            <p className="text-sm text-gray-500 mt-2">Group Project</p>
+            <p className="text-sm text-gray-500 mt-2">Collectively UI built with Javascript and CSS for styling</p>
           </Link>
         </div>
       </div>
